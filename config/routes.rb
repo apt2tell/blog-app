1616cdf542ff072d72af7cbcc133a346f9bdae88
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-18 20:03:47 +0100

    Create migration for Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
