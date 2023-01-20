bcc6496c31ea78a9159b7bbe0fccf7f69f674989
Okechukwu <okei.muokwugwo@gmail.com>
2023-01-20 10:21:05 +0100

    Add methods for post  and recent comment counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  after_create :increment_user_post_count
+
+  # Increment user posts count
+  def increment_user_post_count
+    user.increment!(:posts_count)
+  end
+
+  # Return user most recent comments
+  def recent_comments
+    comments.order(created_at::desc).limit(5)
+  end
 end
