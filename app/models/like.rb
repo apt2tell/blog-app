c32e5f3d5b8d07196c4e69f3b3f1d26a77a29fb4
Okechukwu <okei.muokwugwo@gmail.com>
2023-01-20 10:31:20 +0100

    Implement method for likes counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :increment_like_count
+
+  def increment_like_count
+    post.increment!(:likes_count)
+  end
+
+  private increment_like_count
 end
