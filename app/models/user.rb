fee4252ae47337c56871ef68e6ba7b9780dc0f57
Okechukwu <okei.muokwugwo@gmail.com>
2023-01-20 09:44:24 +0100

    Implement method for recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
+
+  # Most recent posts
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
