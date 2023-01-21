c253d1f98c4e7f63bd749cdf58571beff6ea16c2
Okechukwu <okei.muokwugwo@gmail.com>
2023-01-21 12:29:19 +0100

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   # Most recent posts
   def recent_posts
     posts.order(created_at: :desc).limit(3)
