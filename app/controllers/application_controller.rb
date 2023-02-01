4225fdde8251d7468e22ba4df5de94ad2c1c7462
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-01 16:21:49 +0100

    Update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo, :email, :password ])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo, :email, :password, current_password ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :photo, :email, :password, :current_password ])
   end
 end
