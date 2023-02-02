fde437af7ca98e94a933ef72caec9d2f38b19637
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-02 14:35:19 +0000

    Add route for the destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     root "users#index"
     
     resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :new, :create, :show] do
+      resources :posts, only: [:index, :new, :create, :show, :destroy] do
        resources :comments, only: [:create]
         resources :likes, only: [:create]
     end
