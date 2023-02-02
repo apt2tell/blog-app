a9ae0f2f8a0bce867ca648ab0fd54fbc2c8867bc
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-02 16:46:26 +0100

    Add routes for comment destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index, :new, :create, :show, :destroy] do
-       resources :comments, only: [:create]
+       resources :comments, only: [:create, :destroy]
         resources :likes, only: [:create]
     end
   end
