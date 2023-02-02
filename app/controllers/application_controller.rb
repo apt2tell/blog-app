722102f3210ae3fb22afa6070a2f34a2677f0e05
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-02 15:22:53 +0100

    Catch all cancan errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Catch all CanCan errors and alert the user of the exception
+  rescue_from CanCan::AccessDenied do | exception |
+    redirect_to root_url, alert: exception.message
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo email password])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo email password current_password])
