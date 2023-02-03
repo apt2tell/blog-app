7c92e35a1485bea6c264efd1b6b08de5affd6a55
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-03 10:00:14 +0100

    Generate API controller for users comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::CommentsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
     namespace :api do
       namespace :v1 do
-        resources :users, only: [:index, :show]
+        resources :users, only: [:index, :show] do
       end
     end
   end
