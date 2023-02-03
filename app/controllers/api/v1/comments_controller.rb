4410fc478d8b17e1b08f593f4e03507615615ca3
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-03 09:15:33 +0000

    Add API to comment on a post

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::CommentsController < ActionController::API
     comments = post.comments
     render json: comments, status: :ok
   end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.new(comment_params)
+
+    if comment.save
+      render json: comment, status: :ok
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
 end
