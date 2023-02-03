d17616f9c324d20d2203fd2edddf12bca81a1be8
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-03 09:31:08 +0100

    Add routes for api users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-    root "users#index"
+  root "users#index"
     
-    resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :new, :create, :show, :destroy] do
-       resources :comments, only: [:create, :destroy]
-        resources :likes, only: [:create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :create, :show, :destroy] do
+      resources :comments, only: [:create, :destroy]
+      resources :likes, only: [:create]
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
     end
   end
 end
