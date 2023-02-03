434c36afb7d36ef46ae9dbd7c3ea0809bd29526e
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-03 16:21:46 +0100

    Add test case for users endpoint

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,42 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Api::V1::Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'api/v1/users', type: :request do
+
+  path '/api/v1/users' do
+
+    get('list users') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+    user = User.create(name: "Tom", bio: "Teaccher from Mexico", photo: "http://")
+
+    get('show user') do
+      response(200, 'successful') do
+        let(:id) { user.id } 
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
