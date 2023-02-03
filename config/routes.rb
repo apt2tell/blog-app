2442f46e844469860a40942664201dbedaa4079f
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-03 09:15:52 +0000

    Add API route to create a comment

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show] do
         resources :posts, only: [:index, :show] do
-          resources :comments, only: [:index]
+          resources :comments, only: [:index, :create]
         end
       end
     end
